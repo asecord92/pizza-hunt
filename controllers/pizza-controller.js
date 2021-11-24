5cd6cbf491594911623a8d8abd1041dca999eacb
Adam <ats92@cox.net>
2021-11-24 10:56:11 -0800

    added comment functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,26 +3,37 @@ const { Pizza } = require('../models');
 const pizzaController = {
     getAllPizza(req, res) {
         Pizza.find({})
+        .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+        .select('-__v')
+        .sort({_id: -1})
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
         });
     },
-    getPizzaById({params}, res) {
-        Pizza.findOne({_id: params.id})
-        .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({message: 'no pizza found with this id!'})
-                return;
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+          .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
             }
             res.json(dbPizzaData);
-        })
-        .catch(err => {
+          })
+          .catch(err => {
             console.log(err);
             res.status(400).json(err);
-        })
-    },
+          });
+      },
     createPizza({body}, res) {
         Pizza.create(body)
         .then(dbPizzaData => res.json(dbPizzaData))
